dace73ac9c769fa71b0264c94d4abe4f4b8ecaec
Gibryon Bhojraj <gibryon@protonmail.com>
2022-02-18 13:22:48 -0500

    Update the main App page to host a form with the USWDS form components

---
+++ documentation/Architecture.md
@@ -0,0 +1,8 @@
+# Architecture
+
+## Department for the Protection of Pets architecture
+
+### Existing architecture
+
+The current architecture for DPP involves storing RescuedPet information in an
+FTP server behind PetNet.
--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
-import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Form, TextInput, Button, Header, GovBanner, Label } from '@trussworks/react-uswds'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return (<>
+    <GovBanner></GovBanner>
+    <Header>
+      <div className='header-div'>
+        <img src={logo} alt='logo' height={'100rem'} /><h1 className='header-text'>Department for the Protection of Pets</h1>
+      </div>
+    </Header>
+    <div className='form-div'>
+      <Form onSubmit={() => console.log('submit')}>
+        <h2>Sign Up for Rescue Alerts!</h2>
+        <Label htmlFor='full_name'>Full Name</Label>
+        <TextInput id='full_name' name='full_name' type={'text'} />
+        <Label htmlFor='pet_name'>Pet Name</Label>
+        <TextInput id='pet_name' name='pet_name' type={'text'} />
+        <Label htmlFor='pet_id'>Pet ID</Label>
+        <TextInput id='pet_id' name='pet_id' type={'text'} />
+        <Label htmlFor='phone'>Phone Number</Label>
+        <TextInput id='phone' name='phone' type={'tel'} />
+        <Button type='submit'>Submit</Button>
+      </Form>
     </div>
+  </>
   );
 }
 
